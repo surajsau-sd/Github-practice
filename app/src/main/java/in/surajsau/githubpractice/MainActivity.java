6c922b2e9929ef0a45ebce15192252b18dfd8883
Suraj Sau <suraj.sau@snapdeal.com>
2016-02-19 16:38:51 +0530

    second commit

--- app/src/main/java/in/surajsau/githubpractice/MainActivity.java
+++ app/src/main/java/in/surajsau/githubpractice/MainActivity.java
@@ -1,5 +1,6 @@
 package in.surajsau.githubpractice;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +24,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(getActivityContext(), "Hello World", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private Context getActivityContext() {
+        return MainActivity.this;
     }
 
     @Override
